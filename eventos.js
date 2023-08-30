a163533b9279a2459e3642134f5690faa9877bb2
esequeva <ingenieria.roh@gmail.com>
2023-08-30 13:40:03 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const divSaludo = document.querySelector("div");
 
     divSaludo.addEventListener("dblclick", function(){
-        alert("Hola, soy el div");
+        alert("Hola, soy el div con doble click");
 
 });
 });
